b3d82520b48f5516e1534f62e24964734d8452c7
Lasse <nonspammabile@gmail.com>
2019-05-26 23:28:25 +0200

    Referencing other tables works!

--- src/sources/krav/index.js
+++ src/sources/krav/index.js
@@ -20,26 +20,15 @@ module.exports = function (api, opts) {
         });
 
 
-
-
-
-
-        await base('N100').select({view: 'Prototype'}).eachPage((records, fetchNextPage) => {
-            let count = 0;
-            records.forEach((record) => {
-                const item = record._rawJson;
-
-                krav.addNode({
-                    sequence: count++,
-                    id: item.id,
-                    fields: item.fields,
-                });
-
-                console.log('Retrieved', item.fields.Nr);
-
-            });
-            fetchNextPage();
-        });
+        //krav.addReference('figref', 'Figur')
+/*
+        contentType.addSchemaField('price', ({ graphql }) => ({
+            type: graphql.GraphQLFloat,
+            resolve (node) {
+                return node.fields.unitCost;
+            }
+        }));
+*/
 
          await base('Figurer').select().eachPage((records, fetchNextPage) => {
             records.forEach((record) => {
@@ -50,7 +39,7 @@ module.exports = function (api, opts) {
                     fields: item.fields,
                 });
 
-                console.log('Retrieved', item.fields.FigurNr);
+                console.log('Figur: ', item.id);
 
             });
             fetchNextPage();
@@ -67,7 +56,30 @@ module.exports = function (api, opts) {
                     fields: item.fields,
                 });
 
-                console.log('Retrieved', item.fields.Value);
+                console.log('Tabell: ', item.id);
+
+            });
+            fetchNextPage();
+        });
+
+
+        await base('N100').select({view: 'Prototype'}).eachPage((records, fetchNextPage) => {
+            let count = 0;
+            records.forEach((record) => {
+                const item = record._rawJson;
+
+                krav.addNode({
+                    sequence: count++,
+                    id: item.id,
+                    fields: item.fields,
+                    figref: store.createReference('Figur', item.fields.Figurer),
+                    //figurer: [item.fields.Figurer],
+                });
+
+                console.log('Krav: ', item.id);
+                if(item.fields.Figurer) {
+                    console.log('Has ref: ', item.fields.Figurer);
+                }
 
             });
             fetchNextPage();
