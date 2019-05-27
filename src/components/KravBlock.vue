585932ec9408e2500b02a8d4fcbe12197dc143b0
Lasse <nonspammabile@gmail.com>
2019-05-27 23:09:07 +0200

    More style changes

--- src/components/KravBlock.vue
+++ src/components/KravBlock.vue
@@ -3,21 +3,21 @@
 
     <div class="kravBlock pr-4 border border-gray-300 p-3 mb-4">
 
-        <div class="flex mb-4 text-lg">{{block.Krav}}</div>
-
         <div class="flex text-xs text-gray-500">
             <div class="w-full text-right italic underline">Versjon {{block.Versjon}}</div>
         </div>
 
+        <div class="flex mb-4 text-lg">{{block.Krav}}</div>
+
+
         <div v-if="block.figref.length" class="FIGURE" >
             <a @click="onClick('fig')" class="cursor-pointer"><i class="fas fa-angle-right"></i> Figurer ({{block.figref.length}})</a>
             <div v-bind:id="'ref-'+block.Nr" ref="veiledning" v-bind:class="{hidden: fighidden}">
-                <div v-for="figref in block.figref" class="text-sm text-gray-800">
-                   Figur {{figref.FigurNr }}: {{ figref.Tekst }}
-                    <!-- <g-image :src="figref.Figurbilde[0].url"></g-image> -->
+                <div v-for="figref in block.figref" class="text-sm text-gray-800 ml-4">
                     <div class="w-16 h-12 my-3"
                          v-bind:style="'background-image: url('+figref.Figurbilde[0].thumbnails.small.url+')'">
                     </div>
+                    <p>Figur {{figref.FigurNr }}: {{ figref.Tekst }}</p>
                 </div>
             </div>
         </div>
@@ -28,7 +28,7 @@
 
         <div v-if="block.Tilknyttet_tekst" class="REF">
             <a @click="onClick('txt')" class="cursor-pointer"><i class="fas fa-angle-right"></i> Veiledning</a>
-            <div v-bind:id="'ref-'+block.Nr" ref="veiledning" v-bind:class="{hidden: txthidden}">
+            <div v-bind:id="'ref-'+block.Nr" ref="veiledning" v-bind:class="{hidden: txthidden}" class="ml-4">
                 {{ block.Tilknyttet_tekst }}
             </div>
         </div>
