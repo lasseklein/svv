a1a89a42a5ef8eddb0c59060560abec6f2fc098a
Lasse <nonspammabile@gmail.com>
2019-05-27 15:37:11 +0200

    Start styling krav-tillegg

--- src/components/KravBlock.vue
+++ src/components/KravBlock.vue
@@ -3,29 +3,32 @@
 
     <div class="kravBlock pr-4 border border-gray-300 p-3 mb-4">
 
+        <div class="flex mb-4">{{block.Krav}}</div>
+
         <div class="flex text-xs text-gray-500">
             <div class="w-full text-right italic underline">Versjon {{block.Versjon}}</div>
         </div>
 
-        <div class="flex mb-4">{{block.Krav}}</div>
-
         <div v-if="block.figref.length" class="FIGURE" >
-            <div v-for="figref in block.figref">
-               Figur {{figref.FigurNr }}: {{ figref.Tekst }}
-                <g-image :src="figref.Figurbilde[0].url"></g-image>
-                <div class="w-16 h-12"
-                     v-bind:style="'background-image: url('+figref.Figurbilde[0].thumbnails.small.url+')'">
+            <a @click="onClick('fig')"><i class="fas fa-angle-right"></i> Figurer</a>
+            <div v-bind:id="'ref-'+block.Nr" ref="veiledning" v-bind:class="{hidden: fighidden}">
+                <div v-for="figref in block.figref" class="text-sm text-gray-800">
+                   Figur {{figref.FigurNr }}: {{ figref.Tekst }}
+                    <!-- <g-image :src="figref.Figurbilde[0].url"></g-image> -->
+                    <div class="w-16 h-12 my-3"
+                         v-bind:style="'background-image: url('+figref.Figurbilde[0].thumbnails.small.url+')'">
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div v-if="block.Tabell" class="TABLE text-red">
-            TABELL [{{block.Tabell}}]
+        <div v-if="block.Tabell" class="TABLE">
+            <a @click="onClick('tab')"><i class="fas fa-angle-right"></i> Tabeller</a>
         </div>
 
         <div v-if="block.Tilknyttet_tekst" class="REF cursor-pointer">
-            <a @click="onClick()"><i class="fas fa-angle-right"></i> Veiledning</a>
-            <div v-bind:id="'ref-'+block.Nr" ref="veiledning" v-bind:class="{hidden: hidden}">
+            <a @click="onClick('txt')"><i class="fas fa-angle-right"></i> Veiledning</a>
+            <div v-bind:id="'ref-'+block.Nr" ref="veiledning" v-bind:class="{hidden: txthidden}">
                 {{ block.Tilknyttet_tekst }}
             </div>
         </div>
@@ -41,12 +44,24 @@
         props: ['block'],
         data: function () {
             return {
-                hidden: true,
+                fighidden: true,
+                txthidden: true
             }
         },
         methods: {
-            onClick () {
-                this.hidden = !this.hidden;
+            onClick (i) {
+                switch(i){
+                    case 'fig':
+                        this.fighidden = !this.fighidden;
+                        break;
+                    case 'txt':
+                        this.txthidden = !this.txthidden;
+                        break;
+                    case 'tab':
+                        this.tabhidden = !this.tabhidden;
+                        break;
+                }
+
             }
         }
     };
