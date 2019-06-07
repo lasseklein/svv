6f842b00e262fe1a201e368abdc8a6b385a51386
Lasse <nonspammabile@gmail.com>
2019-06-07 15:28:42 +0200

    Styling av H1-H4 for overskrifter

--- src/components/TitleBlock.vue
+++ src/components/TitleBlock.vue
@@ -1,12 +1,12 @@
 <template>
     <div v-if="block.type==='Kapittel'" class="">
-        <h2 class="title"><span v-if="block.kapittel!=='0'">{{block.kapittel}} </span>{{block.fagtema}}</h2>
+        <h1><span v-if="block.kapittel!=='0'">{{block.kapittel}} </span>{{block.fagtema}}</h1>
     </div>
     <div v-else-if="block.avsnitt.length===1" class="">
-        <h3 class="subtitle">{{block.kapittel}}.{{block.avsnitt}} <span class="ml-3">{{block.fagtema}}</span></h3>
+        <h2>{{block.kapittel}}.{{block.avsnitt}} <span class="ml-3">{{block.fagtema}}</span></h2>
     </div>
     <div v-else class="">
-        <h3 class="subsubtitle">{{block.kapittel}}.{{block.avsnitt}} <span class="ml-3">{{block.fagtema}}</span></h3>
+        <h3>{{block.kapittel}}.{{block.avsnitt}} <span class="ml-3">{{block.fagtema}}</span></h3>
     </div>
 </template>
 
