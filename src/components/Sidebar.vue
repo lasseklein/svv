4c45152a9087072c4ef6d75d39574f8dab62a5bf
Lasse <nonspammabile@gmail.com>
2019-06-07 13:23:01 +0200

    Fjernet prefix for kapittel hvis '0'

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,7 +5,7 @@
         <div class="h-full overflow-y-auto">
             <div v-for="edge in $static.allKrav.edges" :key="edge.node.id">
                 <a v-if="edge.node.type==='Kapittel'" class="block border-t border-gray-medium self-center hover:bg-gray-light" v-bind:href="'/kapittel/'+edge.node.kapittel.toLowerCase()">
-                    <span class="block h-16 px-4 py-2">{{edge.node.kapittel}}. {{edge.node.fagtema}}</span>
+                    <span class="block h-16 px-4 py-2"><span v-if="edge.node.kapittel!=='0'">{{edge.node.kapittel}}. </span>{{edge.node.fagtema}}</span>
                 </a>
                 <!-- <a v-else-if="edge.node.type==='Tittel' && edge.node.avsnitt.length === 1" class="ml-10 block"> {{edge.node.kapittel}}.{{edge.node.avsnitt}} {{edge.node.fagtema}}</a> -->
             </div>
--- src/components/TitleBlock.vue
+++ src/components/TitleBlock.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="block.type==='Kapittel'" class="">
-        <h2 class="text-gray-dark uppercase text-3xl mt-3 mb-6">{{block.kapittel}} {{block.fagtema}}</h2>
+        <h2 class="text-gray-dark uppercase text-3xl mt-3 mb-6"><span v-if="block.kapittel!=='0'">{{block.kapittel}} </span>{{block.fagtema}}</h2>
     </div>
     <div v-else-if="block.avsnitt.length===1" class="">
         <h3 class="text-3xl mt-16 mb-6">{{block.kapittel}}.{{block.avsnitt}} <span class="ml-3">{{block.fagtema}}</span></h3>
