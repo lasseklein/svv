c066213682eeb1cdc2847596208c8c2d1ecaf257
Lasse <nonspammabile@gmail.com>
2019-06-20 15:23:56 +0200

    Referanser på hver sin linje

--- src/components/Reference.vue
+++ src/components/Reference.vue
@@ -1,11 +1,11 @@
 <template>
     <div v-if="block.kravref.length" class="text-sm mt-2">
-        <span v-for="ref in block.kravref" :key="block.kravref.id" class="text-gray-dark underline cursor-pointer">
+        <div v-for="ref in block.kravref" :key="block.kravref.id" class="text-gray-dark underline cursor-pointer mt-2">
 
             <a v-if="ref.type==='Krav'" v-bind:href="'/'+ref.booknr+'/'+ref.kapittel+'#'+ref.id"><i class="fas fa-link text-xl text-gray-dark mr-2"></i>Krav {{ ref.kravID }}</a>
             <a v-else v-bind:href="'/'+ref.booknr+'/'+ref.kapittel+'#'+ref.id"><i class="fas fa-link text-xl text-gray-dark mr-2"></i>{{ ref.booknr[0]}} {{ ref.kapittel }}.{{ ref.avsnitt }}: {{ref.fagtema}}</a>
 
-        </span>
+        </div>
     </div>
 </template>
 
