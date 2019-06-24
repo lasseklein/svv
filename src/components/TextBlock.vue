453a75c0787644a63d79bb3ca3c14e6aac9bf6a2
Lasse <nonspammabile@gmail.com>
2019-06-24 13:41:04 +0200

    Prøver å få netlify til å virke igjen.

--- src/components/TextBlock.vue
+++ src/components/TextBlock.vue
@@ -2,20 +2,6 @@
     <div>
         <div v-html="compiledMarkdown" class="font-sans text-base my-4"></div>
         <Reference v-bind:block="block" />
-
-        <div v-if="block.figref.length || block.tabref.length || block.kravref.length" class="references border-gray-light pt-1 pb-2 my-3 border-t border-b">
-
-
-            <Thumbnail v-bind:item="block.figref" name="Figur" />
-            <Table v-bind:item="block" name="Tabell" />
-            <Reference v-bind:block="block" />
-
-            <div v-if="block.vedlegg.length" v-for="vl in block.vedlegg" :key="vl.filename" class="mt-3">
-                <a v-bind:href="vl.url" class="underline text-gray-dark text-sm "><i class="fas fa-paperclip mr-4"></i>{{vl.filename}}</a>
-            </div>
-
-
-        </div>
     </div>
 
 </template>
@@ -23,8 +9,6 @@
 <script>
     import marked from "marked";
     import Reference from "./Reference";
-    import Thumbnail from "./Thumbnail";
-    import Table     from "./Table";
 
     // TODO: Få inn block og vise figurer og bilder her også.
 
@@ -35,8 +19,6 @@
         components: {
             Reference,
             marked,
-            Thumbnail,
-            Table,
         },
         computed: {
             compiledMarkdown: function(){
