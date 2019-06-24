1fb2f1463bf12c010809281a6da45df221780656
Lasse <nonspammabile@gmail.com>
2019-06-24 16:33:52 +0200

    Fikset doble linker

--- src/components/TextBlock.vue
+++ src/components/TextBlock.vue
@@ -3,17 +3,14 @@
         <div v-html="compiledMarkdown" class="font-sans text-base my-4"></div>
         <Reference v-bind:block="block" />
 
-        <div v-if="block.figref.length || block.tabref.length || block.kravref.length" class="references border-gray-light pt-1 pb-2 my-3 border-t border-b">
-
+        <div v-if="block.figref.length || block.tabref.length || block.kravref.length" class="pt-1 pb-2 my-6">
 
             <Thumbnail v-bind:item="block.figref" name="Figur" v-bind:isActive="true" />
             <Table v-bind:item="block" name="Tabell" v-bind:isActive="true" />
-            <Reference v-bind:block="block" />
 
             <div v-if="block.vedlegg.length" v-for="vl in block.vedlegg" :key="vl.filename" class="mt-3">
                 <a v-bind:href="vl.url" class="underline text-gray-dark text-sm "><i class="fas fa-paperclip mr-4"></i>{{vl.filename}}</a>
             </div>
-
 
         </div>
     </div>
